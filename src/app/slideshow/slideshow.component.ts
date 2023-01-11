0c1c1836185634333e2ab778cdf25db1cf838317
Leonardo van Dahl <111518533+Leonardo190392@users.noreply.github.com>
2023-01-11 19:42:53 +0100

    started header menu

---
+++ src/app/slideshow/slideshow.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-slideshow',
+  templateUrl: './slideshow.component.html',
+  styleUrls: ['./slideshow.component.scss']
+})
+export class SlideshowComponent implements OnInit {
+  images = ['code.jpg','learn.jpg','notebook.jpg'];
+  headlines = [
+    'Bring engineering to the next level', 
+    'Born to code', 
+    'Graduaded at DEV Akadmie'
+  ];
+
+  currentImage = 0;
+  showImage = true;
+
+  ngOnInit() {
+    this.updateImage();
+  }
+
+
+
+  updateImage() {
+
+    setInterval(() => {
+      this.currentImage++;
+      this.currentImage = this.currentImage % this.images.length;
+      this.showImage = false;
+
+      setTimeout(() => {
+        this.showImage = true;
+      }, 10);
+    }, 8000);
+
+  }
+}
